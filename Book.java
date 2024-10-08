a94991a8d581927cf256639087e51a938d1ee3ac
Katie <vaporfrost007@gmail.com>
2024-10-08 16:36:40 -0400

    a lot lol

--- Book.java
+++ Book.java
@@ -11,15 +11,63 @@ class Book
     // The fields.
     private String author;
     private String title;
+    private int pages;
+    private String refNumber;
 
-    /**
-     * Set the author and title fields when this object
-     * is constructed.
-     */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle, int bookPages)
     {
         author = bookAuthor;
         title = bookTitle;
+        pages = bookPages;
+        refNumber = "";
+    }
+
+    public void setRefNumber(String ref)
+    {
+        if(ref.length() >= 3) 
+        {
+         refNumber = ref;
+        }
+        else 
+        {
+            System.out.println("Error! The reference number must be at least 3 characters long.");
+        }
+    }
+    
+    public String getAuthor()
+    {
+        return author;
+    }
+
+    public String getTitle() 
+    {
+        return title;
+    } 
+    
+    public int getPages() 
+    {
+        return pages;
+    } 
+    
+    public String getRefNumber()
+    {
+        return refNumber;
+    }
+    
+    public void printDetails()
+    {
+        System.out.println("Author: " + author);
+        System.out.println("Title: " + title);
+        System.out.println("Pages: " + pages);
+        
+        if(refNumber.length() > 0)
+        {
+            System.out.println("Reference number:" + refNumber);
+        }
+        else
+        {
+            System.out.println("Reference number:" + "ZZZ");
+        }
     }
 
 }
